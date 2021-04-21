269652e30e5809654e219b58d22eed90d98c8fdb
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-04-21 15:02:04 -0300

    Config SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,54 @@
-import Head from "next/head";
+//React
+import React, { useEffect } from "react";
 
-export default function Home() {
-  return <h1>Index</h1>;
+//SSG
+
+export default function Home(props) {
+  /// SPA
+  /*useEffect(() => {
+    fetch("http://localhost:3333/episodes")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);*/
+  /* ------------------------------------------ */
+  ///SSR
+  /*return (
+   <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+   </div> 
+  );*/
+
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+///SSR
+/*
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+  };
+}
+*/
+
+//SSG
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
