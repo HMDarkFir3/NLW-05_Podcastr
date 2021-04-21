b8219a96d75800f3009d5c8413eb659d1e030f60
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-04-21 13:53:13 -0300

    Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+//React
+import React from "react";
+
+//date-fns
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+
+//Style
+import styles from "./styles.module.scss";
+
+export default function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", { locale: ptBR });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+
+//Component
+import Header from "../components/Header";
+
+export default function Home() {
+  return <Header />;
+}
